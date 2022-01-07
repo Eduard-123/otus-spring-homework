60a5073b2db1667b0a1c04946c188c25fdadc08e
Traumerei <eduard.borisov94@gmail.com>
2022-01-07 22:14:06 +0300

    Revert "library webflux"
    This reverts commit ab9d3066

--- hw11/src/main/java/ru/otus/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.library.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import ru.otus.library.model.Book;
-
-public interface BookRepository extends ReactiveMongoRepository<Book, String> {
-}
--- hw11/src/main/java/ru/otus/library/service/BookNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package ru.otus.library.service;
-
-public class BookNotFoundException extends RuntimeException {
-}
