a9cc0c4b92589671c71a0634e685faf2147a025a
Traumerei <eduard.borisov94@gmail.com>
2022-01-07 22:20:21 +0300

    fix......

--- hw11/src/main/java/ru/otus/library/repository/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.library.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import ru.otus.library.model.Author;
-
-
-public interface AuthorRepository extends ReactiveMongoRepository<Author, String> {
-}
--- hw11/src/main/java/ru/otus/library/repository/GenreRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.library.repository;
-
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import ru.otus.library.model.Genre;
-
-public interface GenreRepository extends ReactiveMongoRepository<Genre, String> {
-}
