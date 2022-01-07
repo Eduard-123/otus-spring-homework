0530f66987565cbedbdae2e3f26a668b2696d90b
Traumerei <eduard.borisov94@gmail.com>
2022-01-07 21:40:01 +0300

    library web

---
+++ hw09/src/main/java/ru/otus/library/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package ru.otus.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.otus.library.model.Book;
+import ru.otus.library.model.Comment;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByBook(Book book);
+}
