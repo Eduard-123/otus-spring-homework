48f2183dc6ee66b1e90a5a8867516bf193c5c72b
Traumerei <eduard.borisov94@gmail.com>
2022-05-09 13:27:42 +0300

    docker

---
+++ hw17/src/main/java/ru/otus/homework/controller/CommentController.java
@@ -0,0 +1,29 @@
+package ru.otus.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.otus.homework.domain.Comment;
+import ru.otus.homework.service.CommentService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/comment")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteComment(@PathVariable("id") Long id) {
+        commentService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> createComment(@RequestBody Comment comment) {
+        commentService.addOrSaveComment(comment);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+}
